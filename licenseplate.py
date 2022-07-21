1f3b87216f52aa0949bd8cd87c44fb740c15b0a6
soapp88 <91401787+soapp88@users.noreply.github.com>
2022-07-21 07:17:13 -0700

    Add files via upload

---
+++ licenseplate.py
@@ -0,0 +1,53 @@
+"""
+Copyright MIT BWSI Autonomous RACECAR Course
+MIT License
+Summer 2022
+
+Code Clash #9 - License Plate (licenseplate.py)
+
+
+Author: Wonjun Lee
+
+Difficulty Level: 5/10
+
+Prompt: Jon was speeding on the highway with his RACECAR, and the highway camera has taken a picture 
+of the RACECAR’s number plate. However, some characters on the plate are blurry. Please write a function 
+that returns the number of possible combinations of the number plate. The number plate for his RACECAR
+consists of 7 distinct characters, starting with 3 distinct alphabets and ending with 4 distinct numbers. 
+The input will be passed as a string and any blurred characters will be written as ‘.’
+
+Test Cases: 
+Input: “ABC123.” ; Output: 7
+Input: “.ON0123” ; Output: 24
+Input: “.BC.234” ; Output: 168
+"""
+
+class Solution:
+    def licensePlate(self,str):
+        # type str: string
+        # return: int
+        
+        # TODO: Write code below to return an int with the solution to the prompt
+        combinations = 0
+
+        for i in range(7):
+            if i < 3 and str[i] == '.' and combinations == 0:
+                combinations += 26
+            elif i < 3 and str[i] == '.' and combinations != 0 :
+                combinations *= 26
+            
+
+            elif i > 3 and str[i] == '.':
+                combinations *= 10
+        
+        return combinations
+
+
+def main():
+    string1 = input()
+    tc1 = Solution()
+    ans = tc1.licensePlate(string1)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
